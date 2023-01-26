9c7ac6fd7d78b7c5030affaa6700feb7612128d1
KhaledAlabssi <khaled.absi5@gmail.com>
2023-01-26 07:27:39 +0100

    updating package json to common type

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import jsonServer from 'json-server'
-
+const jsonServer = require("json-server")
 const server = jsonServer.create()
 const router = jsonServer.router("db.json")
 const middlewares = jsonServer.defaults()
+// const dotenv = require("dotenv")
+// dotenv.config()
 const port = process.env.PORT || 3001
 
 server.use(middlewares)
